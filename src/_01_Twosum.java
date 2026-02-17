832eee4d2e62a18b6da6e7d48c38fcec276d1303
skillsstackpro <rohankataria48@gmail.com>
2026-02-17 21:09:16 +0500

    finl code for today

--- src/_01_Twosum.java
+++ src/_01_Twosum.java
@@ -13,8 +13,7 @@ public class _01_Twosum {
             }
             return new int[] {};
         }
-
-
+        
     public static void main(String[] args) {
         int nums[]= {2,7,11,15};
         int target =9;
