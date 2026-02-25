bb6914dffb6abe9b12a3ae11cceb311396f8538e
skillsstackpro <rohankataria48@gmail.com>
2026-02-25 15:59:12 +0500

    final with median logic

--- src/_04_mediantwosortedarray.java
+++ src/_04_mediantwosortedarray.java
@@ -4,9 +4,8 @@ public class _04_mediantwosortedarray {
         int m=num2.length;
 
         int merged[]=new int[n+m];
-
         int i=0,j=0,k=0;
-
+//      apply merge sort
         while(i<n && j<m){
             if(num1[i]<num2[j]){
                 merged[k]=num1[i];
@@ -18,22 +17,32 @@ public class _04_mediantwosortedarray {
             }
             k++;
         }
+        //shift remaining elements of num1
         while(i<n){
             merged[k]=num1[i];
             i++;
             k++;
+
         }
+        //shift remaining elements of num2
         while(j<m){
             merged[k]=num2[j];
             j++;
             k++;
         }
+
         System.out.print("sorted array is ");
         for(int x=0;x<merged.length;x++){
             System.out.print(merged[x]);
             System.out.print(" ");
         }
-        return 1;
+        int p = merged.length;
+        if(p%2==0){
+            return ( merged[p/2-1]+merged[p/2])/2.0;
+        }
+        else{
+            return (double)merged[p/2];
+        }
     }
 
     public static void main(String[] args) {
@@ -43,7 +52,7 @@ public class _04_mediantwosortedarray {
         _04_mediantwosortedarray sol = new _04_mediantwosortedarray();
 
        double result= sol.mergesort(num1,num2);
-
-
+        System.out.println("\nmedian is "+result);
+        
     }
 }
