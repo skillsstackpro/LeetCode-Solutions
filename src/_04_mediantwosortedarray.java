bb1fc7f16ecc37914aa8b8afadc84ded9ec71355
skillsstackpro <rohankataria48@gmail.com>
2026-02-25 15:54:51 +0500

    merge and sort two array

---
+++ src/_04_mediantwosortedarray.java
@@ -0,0 +1,49 @@
+public class _04_mediantwosortedarray {
+    public double mergesort(int num1[],int num2[]){
+        int n = num1.length;
+        int m=num2.length;
+
+        int merged[]=new int[n+m];
+
+        int i=0,j=0,k=0;
+
+        while(i<n && j<m){
+            if(num1[i]<num2[j]){
+                merged[k]=num1[i];
+                i++;
+            }
+            else{
+                merged[k]=num2[j];
+                j++;
+            }
+            k++;
+        }
+        while(i<n){
+            merged[k]=num1[i];
+            i++;
+            k++;
+        }
+        while(j<m){
+            merged[k]=num2[j];
+            j++;
+            k++;
+        }
+        System.out.print("sorted array is ");
+        for(int x=0;x<merged.length;x++){
+            System.out.print(merged[x]);
+            System.out.print(" ");
+        }
+        return 1;
+    }
+
+    public static void main(String[] args) {
+        int num1[]={1,2};
+        int num2[]={3,4};
+
+        _04_mediantwosortedarray sol = new _04_mediantwosortedarray();
+
+       double result= sol.mergesort(num1,num2);
+
+
+    }
+}
