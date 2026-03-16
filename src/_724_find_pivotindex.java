d13ffad3f3f47c803d712914dd60189ab7934b61
skillsstackpro <rohankataria48@gmail.com>
2026-03-16 22:39:28 +0500

    set pivotindex

--- src/_724_find_pivotindex.java
+++ src/_724_find_pivotindex.java
@@ -1,9 +1,11 @@
 public class _724_find_pivotindex {
         public int pivotIndex(int[] nums) {
             int totalsum=0,leftsum=0,rightsum=0;
+
             for(int i=0;i<nums.length;i++){
                 totalsum+=nums[i];
             }
+            
             for(int j=0;j<nums.length;j++){
                 rightsum=totalsum-leftsum-nums[j];
                 if(rightsum==leftsum){
